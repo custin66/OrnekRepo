a6f39c2a97ccb31456567567aa71911fdc392df8
Şerafettin Can ERDİNÇ <77841609+custin66@users.noreply.github.com>
2021-03-28 22:34:21 +0300

    Update FirstScript.cs

--- GitHub Deneme/Assets/Scripts/FirstScript.cs
+++ GitHub Deneme/Assets/Scripts/FirstScript.cs
@@ -7,7 +7,8 @@ public class FirstScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position += Vector3.one;
+       // transform.position += Vector3.one;
+       transform.position += Vector3.right;
     }
 
     // Update is called once per frame
